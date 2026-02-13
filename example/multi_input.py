c4a2818c696f00bf973d61a3b246d0eac1a6614c
$ Blaze <140303092+OfficialDex@users.noreply.github.com>
2026-02-13 19:30:30 +0530

    Create multi_input.py with sample phrases
    Add multi_input.py to demonstrate lemonify usage

---
+++ example/multi_input.py
@@ -0,0 +1,14 @@
+from lemonify import lemon
+
+samples = [
+    "have a nice day",
+    "you are worthless",
+    "thats sexy as hell",
+    "i will kill you",
+    "hello friend"
+]
+
+for t in samples:
+    print(t)
+    print(lemon(t))
+    print()
